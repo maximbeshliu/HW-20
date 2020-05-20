28011726aa42f5e2e541858e62ce365ec5f4dd6a
Maximbeshliu <maximbeshliu@yahoo.com>
2020-05-21 00:20:05 +0300

    git commit

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const completedTodos = document.getElementById('completed-todos');
 
 
 addButton.addEventListener('click', addItemToList);
+addButton.addEventListener('click', function () {
+    if (event.target === addButton) {
+        characterCount.innerText = '';
+        totalTodos.innerText = "Total tasks = " + todoList.childNodes.length;
+    }
+});
 todoList.addEventListener('click', handleItemClick);
 todoInput.addEventListener('keydown', handleInputBarCharacter);
 
@@ -28,7 +34,13 @@ function handleItemClick(event) {
     if (event.target.classList.contains("remove")) {
         listItem.remove();
         totalTodos.innerText = "Total tasks = " + ((todoList.childNodes.length + +'1') - +'1');
-        completedTodos.innerText = " vs Completed tasks = " + (((listCheckboxStatus.length + +'1') - +'1') - +'1');
+        completedTodos.innerText = " vs Completed tasks = " + ((listCheckboxStatus.length + +'1') - +'1');
+        ////Remove from localstorage 
+        let tasks = JSON.parse(localStorage.getItem('todos'));
+        for (let i = 0; i < tasks.length; i++) {
+            tasks.splice(tasks[i].id, 1);
+        }
+        localStorage.setItem('todos', JSON.stringify(tasks));
     }
 
     if (event.target.classList.contains("tick")) {
@@ -39,35 +51,51 @@ function handleItemClick(event) {
 }
 
 
-function addItemToList() {
-    if (!todoInput.value) return;
+function addItemToList(text, status = false) {
+    if (!todoInput.value && !text) return;
 
     const listItem = document.createElement('li');
     const listItemRemoveBtn = document.createElement('button');
     const listCheckboxStatus = document.createElement('input');
     const listTextSpan = document.createElement('span');
 
+
+
     listItem.classList.add('todolist__item');
 
     listItemRemoveBtn.innerText = 'x';
     listItemRemoveBtn.setAttribute('class', 'remove');
-    listTextSpan.innerText = todoInput.value;
+    if (todoInput.value) {
+        listTextSpan.innerText = todoInput.value;
+    } else { listTextSpan.innerText = text; }
     listCheckboxStatus.type = 'checkbox';
+    listCheckboxStatus.checked = status;
     listCheckboxStatus.setAttribute('class', 'tick');
 
     listItem.append(listCheckboxStatus);
     listItem.append(listTextSpan);
     listItem.append(listItemRemoveBtn);
 
+
+
+
+    let tasks = JSON.parse(localStorage.getItem('todos'));
+    tasks == null ? tasks = [] : tasks;
+    tasks.push({
+        id: tasks.length,
+        text: todoInput.value,
+        status: false
+    });
+    localStorage.setItem('todos', JSON.stringify(tasks));
+
+
     todoInput.value = '';
     todoList.append(listItem);
-
-    if (event.target === addButton) {
-        characterCount.innerText = '';
-        totalTodos.innerText = "Total tasks = " + todoList.childNodes.length;
-        return;
-    }
 }
 
+let tasks = JSON.parse(localStorage.getItem('todos'));
 
 
+for (let i = 0; i < tasks.length; i++) {
+    addItemToList(tasks[i].text, tasks[i].status);
+}
