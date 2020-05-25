5847a533b964365f8ab3314deae3cde1e9847fe5
Maximbeshliu <maximbeshliu@yahoo.com>
2020-05-25 16:05:53 +0300

    git commit

--- index.js
+++ index.js
@@ -27,34 +27,45 @@ function handleInputBarCharacter(event) {
 }
 
 function handleItemClick(event) {
-    let listItem = event.target.closest('li');
 
     let listCheckboxStatus = document.querySelectorAll('input[type="checkbox"]:checked');
+    let tasks = JSON.parse(localStorage.getItem('todos'));
+    let itemId = parseInt(event.target.closest('li').dataset.todoid);
 
     if (event.target.classList.contains("remove")) {
-        listItem.remove();
+        event.target.closest('li').remove();
         totalTodos.innerText = "Total tasks = " + ((todoList.childNodes.length + +'1') - +'1');
-        completedTodos.innerText = " vs Completed tasks = " + ((listCheckboxStatus.length + +'1') - +'1');
-        ////Remove from localstorage 
-        let tasks = JSON.parse(localStorage.getItem('todos'));
+        completedTodos.innerText = "  Completed tasks = " + ((listCheckboxStatus.length + +'1') - +'1');
+        let newStorageArray = [];
         for (let i = 0; i < tasks.length; i++) {
-            tasks.splice(tasks[i].id, 1);
+            if (itemId !== tasks[i].id) {
+                newStorageArray.push(tasks[i]);
+            }
+            localStorage.setItem('todos', JSON.stringify(newStorageArray));
         }
-        localStorage.setItem('todos', JSON.stringify(tasks));
+
     }
 
     if (event.target.classList.contains("tick")) {
-        listItem.classList.toggle('complete');
-        completedTodos.innerText = " vs Completed tasks = " + ((listCheckboxStatus.length + +'1') - +'1');
-        return;
+        event.target.closest('li').classList.toggle('complete');
+        completedTodos.innerText = "  Completed tasks = " + ((listCheckboxStatus.length + +'1') - +'1');
+        let newStorageArray = [];
+        for (let i = 0; i < tasks.length; i++) {
+            if (itemId === tasks[i].id) {
+                tasks[i].status = !tasks[i].status;
+            }
+            newStorageArray.push(tasks[i]);
+
+        }
+        localStorage.setItem('todos', JSON.stringify(newStorageArray));
     }
 }
 
 
-function addItemToList(text, status = false) {
+function addItemToList(text, status = false, id) {
     if (!todoInput.value && !text) return;
 
-    const listItem = document.createElement('li');
+    let listItem = document.createElement('li');
     const listItemRemoveBtn = document.createElement('button');
     const listCheckboxStatus = document.createElement('input');
     const listTextSpan = document.createElement('span');
@@ -65,12 +76,12 @@ function addItemToList(text, status = false) {
 
     listItemRemoveBtn.innerText = 'x';
     listItemRemoveBtn.setAttribute('class', 'remove');
-    if (todoInput.value) {
-        listTextSpan.innerText = todoInput.value;
-    } else { listTextSpan.innerText = text; }
     listCheckboxStatus.type = 'checkbox';
     listCheckboxStatus.checked = status;
+    status ? listItem.classList.add('complete') : null;
     listCheckboxStatus.setAttribute('class', 'tick');
+    itemId = id;
+    listItem.setAttribute('data-todoid', id);
 
     listItem.append(listCheckboxStatus);
     listItem.append(listTextSpan);
@@ -79,14 +90,22 @@ function addItemToList(text, status = false) {
 
 
 
+
     let tasks = JSON.parse(localStorage.getItem('todos'));
     tasks == null ? tasks = [] : tasks;
-    tasks.push({
-        id: tasks.length,
-        text: todoInput.value,
-        status: false
-    });
-    localStorage.setItem('todos', JSON.stringify(tasks));
+
+
+    if (todoInput.value) {
+        let id = tasks.length;
+        listTextSpan.innerText = todoInput.value;
+        tasks.push({
+            id: id,
+            text: todoInput.value,
+            status: false
+        });
+        localStorage.setItem('todos', JSON.stringify(tasks));
+        itemId = id;
+    } else { listTextSpan.innerText = text; }
 
 
     todoInput.value = '';
@@ -97,5 +116,5 @@ let tasks = JSON.parse(localStorage.getItem('todos'));
 
 
 for (let i = 0; i < tasks.length; i++) {
-    addItemToList(tasks[i].text, tasks[i].status);
+    addItemToList(tasks[i].text, tasks[i].status, tasks[i].id);
 }
